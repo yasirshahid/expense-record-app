0068c92c849ada79456373dc86b1a5e3a0e9adad
yasirshahid <66242488+yasirshahid@users.noreply.github.com>
2020-06-19 09:57:24 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+//Imports Components
+
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      This is a test 
+    <div >
+      <Header/>
     </div>
   );
 }
