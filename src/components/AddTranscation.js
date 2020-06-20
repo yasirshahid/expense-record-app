44f28e937f9a88fbf5ceb675a83e9e3b176f9912
yasirshahid <66242488+yasirshahid@users.noreply.github.com>
2020-06-20 01:59:29 -0700

    update all

--- src/components/AddTranscation.js
+++ src/components/AddTranscation.js
@@ -5,7 +5,23 @@ const AddTransaction = () =>{
         <div>
             <h3> Add New Transaction </h3>
             <form>
-                
+                <div className="from-control">
+                    <lable htmlFor="description">
+                        Description
+                    </lable>
+                    <input type="text" required
+                           id="description"
+                           placeholder="Details of transaction"
+                    />
+                    <lable htmlFor="transactionamount">
+                        Transaction Amount
+                    </lable>
+                    <input type="number" required
+                           id="transactionamount"
+                           placeholder="Enter Transaction Amount"                   
+                    />
+                </div>
+                <button className="btn">Add Transaction</button>
             </form>
         </div>
 
